db89d25b18a36b41f790921f00f3fc65abea53ab
anisphia-palettia <euphyllia.tomodachi@gmail.com>
2025-07-07 17:06:56 +0700

    add new group route

--- src/route/r_groups.handler.ts
+++ src/route/r_groups.handler.ts
@@ -1,16 +1,56 @@
 import {LocalHono} from "@/types/LocalHono.ts";
-import WhatsappService from "@/service/whatsapp-service.ts";
-import {handleServiceResponse} from "@/utils/handle-service-response.ts";
-import {appConfig} from "@/config/app-config.ts";
+import {GroupService} from "@/service/group-service.ts";
+import {sendError, sendSuccess} from "@/utils/response.ts";
+import {HTTPException} from "hono/http-exception";
 
 const r_groupsHandler = new LocalHono()
 
 r_groupsHandler.get("",
     async (c) => {
-        const sessionId = appConfig.sessionId
-        const response = await WhatsappService(sessionId).chat.group()
-        return handleServiceResponse(c, response)
+        const groups = await GroupService.all()
+        return sendSuccess(c, {
+            message: "Success get all groups",
+            data: groups
+        })
     }
 )
+
+r_groupsHandler.get("/combined",
+    async (c) => {
+        const groups = await GroupService.combinedAll()
+        return sendSuccess(c, {
+            message: "Success get all groups",
+            data: groups.map(group => ({
+                id: group.id,
+                name: group.name,
+                coordinates: group.coordinates.map(coordinate => ({
+                    latitude: coordinate.lat,
+                    longitude: coordinate.long,
+                    image_name: coordinate.imagePath,
+                }))
+            }))
+        })
+    })
+
+r_groupsHandler.get("/:chatId", async (c) => {
+    const chatId = c.req.param("chatId")
+    const group = await GroupService.findByChatIdAndCoordinates(chatId)
+    if (!group) {
+        throw new HTTPException(404, {message: "Group not found"})
+    }
+    return sendSuccess(c, {
+        message: "Success get group by id",
+        data: {
+            id: group.id,
+            name: group.name,
+            chatId: group.chatId,
+            coordinates: group.coordinates.map(coordinate => ({
+                latitude: coordinate.lat,
+                longitude: coordinate.long,
+                image_name: coordinate.imagePath,
+            }))
+        }
+    })
+})
 
 export default r_groupsHandler
--- src/service/group-service.ts
+++ src/service/group-service.ts
@@ -1,24 +1,46 @@
-import { prisma } from "@/lib/prisma.ts";
+import {prisma} from "@/lib/prisma.ts";
 
 export const GroupService = {
-  async create(data: { name: string; chatId: string }) {
-    return prisma.group.create({
-      data,
-    });
-  },
-  async getByChatId(chatId: string) {
-    return prisma.group.findUnique({
-      where: { chatId },
-    });
-  },
-  async updateById(id: number, name: string) {
-    return prisma.group.update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-      },
-    });
-  },
+    async create(data: { name: string; chatId: string }) {
+        return prisma.group.create({
+            data,
+        });
+    },
+    async getByChatId(chatId: string) {
+        return prisma.group.findUnique({
+            where: {chatId},
+        });
+    },
+    async updateById(id: number, name: string) {
+        return prisma.group.update({
+            where: {
+                id,
+            },
+            data: {
+                name,
+            },
+        });
+    },
+
+    async all() {
+        return prisma.group.findMany();
+    },
+
+    async findByChatIdAndCoordinates(chatId: string) {
+        return prisma.group.findUnique({
+            where: {chatId},
+            include: {
+                coordinates: true,
+            },
+        });
+    },
+
+    async combinedAll() {
+        return prisma.group.findMany({
+                include: {
+                    coordinates: true,
+                },
+            }
+        )
+    }
 };
