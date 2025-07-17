66ccb68d6ad8c2b09ebdc40e2cab58b1403aac90
anisphia-palettia <euphyllia.tomodachi@gmail.com>
2025-07-17 09:32:07 +0700

    bug

--- src/utils/notify-user.ts
+++ src/utils/notify-user.ts
@@ -1,9 +1,9 @@
-import WhatsappService from "@/service/whatsapp-service";
 import type {IWhatsappWebhookMessage} from "@/types/whatsapp";
+import {WhatsappService} from "@/service/whatsapp-service";
 
 export async function notifyUser(message: string, data: IWhatsappWebhookMessage) {
     try {
-        await WhatsappService().message.text(message, data.msg.key.remoteJid!, data.msg);
+        await WhatsappService.message.text(message, data.msg.key.remoteJid!, data.msg);
     } catch (error) {
         console.error("Gagal mengirim pesan ke pengguna:", error);
     }
